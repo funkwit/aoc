29c7ca149b4fa98b9b7c9197ba8d693351d2a283
Paul Cowan <paul@custardsource.com>
2017-12-24 19:48:11 +1100

    Day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -3,22 +3,32 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 
+fn sort(s: &str) -> String {
+	let mut chars = s.chars().collect::<Vec<char>>();
+	chars.sort();
+	chars.into_iter().collect::<String>()
+}
+
 fn main() {
 	let f = File::open("day4input.txt").unwrap();
-    let file = BufReader::new(&f);
-    let mut valid_count = 0;
-    for line in file.lines() {
-    	let mut line = line.unwrap();
-    	println!("Line: {}", line);
-    	let mut parts = line.split_whitespace();
-  	    let mut words = HashSet::new();
-  	    let mut valid = true;
+	let file = BufReader::new(&f);
+	let mut valid_count = 0;
+	for line in file.lines() {
+		let mut line = line.unwrap();
+		println!("Line: {}", line);
+		let mut parts = line.split_whitespace();
+		let mut word;
+  		let mut words = HashSet::new();
+  		let mut valid = true;
 		for p in parts {
-			if words.contains(p) {
-				println!("Duplicate word {}", p);
+			// For part a, remove sort
+			word = sort(p);
+
+			if words.contains(&word) {
+				println!("Duplicate word {}", word);
 				valid = false;
 			}
-			words.insert(p);
+			words.insert(word);
 		}
 		if valid {
 			valid_count = valid_count + 1;
