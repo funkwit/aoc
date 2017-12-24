7683811898d946fe48d3984a892e95d2b64c6562
Paul Cowan <paul@custardsource.com>
2017-12-24 19:59:13 +1100

    Day 5, part 1

---
+++ src/day5.rs
@@ -0,0 +1,24 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+fn main() {
+	let f = File::open("day5input.txt").unwrap();
+	let file = BufReader::new(&f);
+	let mut vec = Vec::new();
+	for line in file.lines() {
+		let n = line.unwrap().parse::<i32>().unwrap();
+		vec.push(n);
+	}
+	println!("Line count {}", vec.len());
+
+	let mut steps: i32 = 0;
+	let mut index: i32 = 0;
+	while index >= 0 && (index as usize) < vec.len() {
+		steps = steps + 1;
+		let jump = vec[index as usize];
+		vec[index as usize] = jump + 1;
+		index = index + jump;
+	}
+	println!("Broke out in {} steps", steps);
+}
